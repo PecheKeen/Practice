c57d4d0785512df87f19b90d2707b2809b76d7f3
Jeremy Holland <jpholland05@gmail.com>
2023-03-02 21:59:14 -0500

    updated meme component

--- learn-react/scrimba/meme-generator/src/components/Meme.jsx
+++ learn-react/scrimba/meme-generator/src/components/Meme.jsx
@@ -9,13 +9,16 @@ function Meme() {
         randomImage: "http://i.imgflip.com/1bij.jpg",
     });
 
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
     function getMemeImage() {
-        const memesArray = memesData.data.memes;
+        const memesArray = allMemeImages.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url;
         setMeme((prevState) => {
             return {
                 ...prevState,
-                randomImage: memesArray[randomNumber].url,
+                randomImage: url,
             }
         })
     }
